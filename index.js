b6ad0bff78c3493ca17629d40c50916f47450067
Walker Malling <walker.malling@gmail.com>
2016-07-30 16:10:25 -0700

    add file scanning
    add async dependency
    use async to scan matching files for port info

--- index.js
+++ index.js
@@ -1,37 +1,90 @@
 const fs = require('fs');
 const curry = require('ramda').curry;
+const async = require('async');
 
-function returnFullPath(path, fileName) {
-  return `${path}/${fileName}`;
+function logResults(results) {
+  console.log(results.path);
+  Object.keys(results.files).forEach(function logFileResults(fileName) {
+    console.log(`├──${fileName}`);
+    if (results.files[fileName]) {
+      results.files[fileName].scanResult.forEach(function logLine(line) {
+        console.log(`|  ├──${line}`);
+      });
+    }
+  });
 }
 
-const fullPath = curry(returnFullPath);
-
-function logFile(fileName) {
-  console.log(`├──${fileName}`);
+function generateFileScan(path, fileName) {
+  return function scanFile (callBack) {
+    const filePath = `${path}/${fileName}`;
+    fs.readFile(filePath, { encoding: 'utf-8' }, function taskCallBack(err, data) {
+      if (err) {
+        console.log({ warning: `Error reading ${filePath}`, code: err.code });
+        callBack(err);
+      } else {
+        const scanResult = data.split('\n').filter(function findPortConfig(line) {
+          return line.indexOf('port') > -1 ||
+            line.indexOf('localhost') > -1;
+        });
+        callBack(null, scanResult);
+      }
+    });
+  };
 }
 
-function logDotFiles(path) {
+function generateFileScanTasks(path, files) {
+  const tasks = {};
+  files.forEach(function generateTask(fileName) {
+    tasks[fileName] = generateFileScan(path, fileName);
+  });
+  return tasks;
+}
+
+function inspectDotFiles(path, files) {
+  var directoryResults = {
+    path: path,
+    files: {},
+  };
+
+  const tasks = generateFileScanTasks(path, files);
+
+  async.parallel(tasks, function asyncDone(err, results) {
+    if (err) {
+      console.log(`error processing ${path}`);
+    } else {
+      Object.keys(results).forEach(function assignScanResults(key) {
+        if (results[key] && results[key].length) {
+          directoryResults.files[key] = {
+            scanResult: results[key],
+          };
+        }
+      });
+      logResults(directoryResults);
+    }
+  });
+}
+
+function lookForProjectDotfiles(path) {
   fs.readdir(path, function callBack(err, files) {
     if (err) {
       console.log(err);
       return;
     }
     const dotfiles = files.filter(function findDotFiles(fileName) {
-      return fileName[0] === '.';
+      return fileName[0] === '.' &&
+        fileName[fileName.length - 1] !== '~' &&
+        fileName.indexOf('.git') === -1 &&
+        fileName.indexOf('ignore') === -1;
     });
-    console.log(path);
-    dotfiles.map(logFile);
+    inspectDotFiles(path, dotfiles);
   });
 }
 
 function getHandler(path) {
   return function scanFileName(fileName) {
     if (fileName === '.git') {
-      // search siblings for .env
-      logDotFiles(path);
+      lookForProjectDotfiles(path);
     } else {
-      // keep crawling path
       crawl(`${path}/${fileName}`);
     }
   };
